6fab06fa2c058ff67a42b0ed56a708fd6e99b621
sy264115809 <264115809@qq.com>
2018-01-20 23:28:40 +0800

    add admin_required filter

---
+++ app/handlers/helper/decorators.py
@@ -0,0 +1,16 @@
+from functools import wraps
+from flask_restful import abort
+from flask_login import current_user
+
+
+def admin_required(func):
+    @wraps(func)
+    def decorator(*args, **kwargs):
+        if not current_user.is_authenticated:
+            abort(401)
+        elif not current_user.is_admin:
+            abort(401)
+        else:
+            func(*args, **kwargs)
+
+    return decorator
